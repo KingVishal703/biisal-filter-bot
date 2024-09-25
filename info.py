bee97dd2caef87ae3fc962698b12f2f14f1c705e
KingVishal703 <vishal21wankhede@gmail.com>
2024-09-25 18:42:52 +0530

    Update info.py

--- info.py
+++ info.py
@@ -17,7 +17,7 @@ API_ID = int(environ.get('API_ID', ''))
 API_HASH = environ.get('API_HASH', '')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1029462448').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1029462448 5796857876').split()]
 USERNAME = environ.get('USERNAME', "https://telegram.me/RW7038")
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002207449255'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/+ujVil_zWT7MxODZl')
